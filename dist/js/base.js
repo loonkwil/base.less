f582c218b601190b460439093a58328df220cfad
Peter Simon <simonpepe@gmail.com>
2015-06-15 11:52:28 +0200

    Release v0.3.4

--- dist/js/base.js
+++ dist/js/base.js
@@ -132,3 +132,95 @@
         }
     };
 }());
+
+/*globals base */
+(function() {
+    'use strict';
+
+    /**
+     * Show or hide an element (with the `hidden` attribute)
+     *
+     * @param {HTMLElement} $el
+     * @param {boolean=} visible
+     * @return {boolean}
+     */
+    base.toggle = function($el, visible) {
+        if (typeof visible !== 'undefined') {
+            visible = !visible;
+        }
+
+        var afterToggle = !base.toggleAttr($el, 'hidden', visible);
+
+        base.trigger('base:toggle', $el, { visible: afterToggle });
+        if (afterToggle) {
+            base.trigger('base:toggle:show', $el);
+        } else {
+            base.trigger('base:toggle:hide', $el);
+        }
+    };
+
+
+    base.onReady(function whenDOMIsReady() {
+        base.onClick(function clickEventHandler(e) {
+            var $el = e.target;
+            if (!$el.hasAttribute('data-toggle')) { return; }
+
+            var targetSelector = $el.getAttribute('data-toggle');
+            var target = base.$(targetSelector);
+            if (!target) { return; }
+
+            base.toggle(target);
+
+            e.stopPropagation();
+            return e.preventDefault();
+        });
+    });
+}());
+
+/*globals base */
+(function() {
+    'use strict';
+
+    /**
+     * @param {HTMLElement} $modal
+     */
+    var closeModal = function($modal) {
+        base.toggle($modal, false);
+    };
+
+    /**
+     * @param {MouseEvent} e
+     */
+    var modalClickEventHandler = function(e) {
+        var outsideClick = e.target.classList.contains('modal');
+        if (!outsideClick) {
+            return;
+        }
+
+        closeModal(this);
+
+        e.stopPropagation();
+        return e.preventDefault();
+    };
+
+
+    base.onReady(function whenDOMIsReady() {
+        var $body = base.$('body');
+
+        base.on('base:toggle', function toggleEventHandler(e) {
+            var $el = e.target;
+            if (!$el.classList.contains('modal')) { return; }
+
+            var isVisible = e.detail.visible;
+            if (isVisible) {
+                $body.classList.add('modal-open');
+                base.onClick(modalClickEventHandler, $el);
+            } else {
+                $body.classList.remove('modal-open');
+                base.off('click', modalClickEventHandler, $el);
+            }
+
+            return e.stopPropagation();
+        });
+    });
+}());
